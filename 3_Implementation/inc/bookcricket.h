2349db2ff3c9a6ca469dc851ad41ae3d7a42ffe3
sankhalapranav <94311905+sankhalapranav@users.noreply.github.com>
2021-11-23 22:20:49 +0530

    Create bookcricket.h

---
+++ 3_Implementation/inc/bookcricket.h
@@ -0,0 +1,18 @@
+/**
+ * @file bookcricket.h
+ * @author Pranav
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifdef __BOOKCRICKET_H__
+#define __BOOKCRICKET_H__
+
+int GetPlayerToss(char player_1[],char player_2[],int oversToplay);
+
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
+
+#endif
